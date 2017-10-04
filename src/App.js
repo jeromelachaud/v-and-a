103875557cb4a721b3686f9857253b2ba7efb899
Jerome Lachaud <jerome.lachaud@gmail.com>
2017-10-04 17:34:38 +0100

    back-up commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
@@ -14,8 +15,9 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
+
 }
 
-export default App;
+export default App
