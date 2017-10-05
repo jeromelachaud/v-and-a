1cc79b8d9997fcbbae92077145b001ecabb5c996
Jerome Lachaud <jerome.lachaud@gmail.com>
2017-10-05 09:33:59 +0100

    hygiene, linting

--- src/Logo.js
+++ src/Logo.js
@@ -8,40 +8,40 @@ export class Logo extends Component {
     anime.timeline({loop: false})
       .add({
         targets: '.ml5 .line',
-        opacity: [0.5,1],
-        scaleX: [0, 1],
-        easing: "easeInOutExpo",
-        duration: 700
+        opacity: [ 0.5, 1 ],
+        scaleX: [ 0, 1 ],
+        easing: 'easeInOutExpo',
+        duration: 700,
       }).add({
         targets: '.ml5 .line',
         duration: 600,
-        easing: "easeOutExpo",
-        translateY: function(e, i, l) {
-          var offset = -0.625 + 0.625*2*i;
-          return offset + "em";
-        }
+        easing: 'easeOutExpo',
+        translateY: function (e, i, l) {
+          let offset = -0.625 + 0.625 * 2 * i
+          return `${offset}em`
+        },
       }).add({
         targets: '.ml5 .ampersand',
-        opacity: [0,1],
-        scaleY: [0.5, 1],
-        easing: "easeOutExpo",
+        opacity: [ 0, 1 ],
+        scaleY: [ 0.5, 1 ],
+        easing: 'easeOutExpo',
         duration: 600,
-        offset: '-=600'
+        offset: '-=600',
       }).add({
         targets: '.ml5 .letters-left',
-        opacity: [0,1],
-        translateX: ["0.5em", 0],
-        easing: "easeOutExpo",
+        opacity: [ 0, 1 ],
+        translateX: [ '0.5em', 0 ],
+        easing: 'easeOutExpo',
         duration: 600,
-        offset: '-=300'
+        offset: '-=300',
       }).add({
         targets: '.ml5 .letters-right',
-        opacity: [0,1],
-        translateX: ["-0.5em", 0],
-        easing: "easeOutExpo",
+        opacity: [ 0, 1 ],
+        translateX: [ '-0.5em', 0 ],
+        easing: 'easeOutExpo',
         duration: 600,
-        offset: '-=600'
-      });
+        offset: '-=600',
+      })
 
   }
 
