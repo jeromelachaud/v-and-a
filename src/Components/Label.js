2487884e424e5e49e5cc7d966a59cd93ca930d0b
Jerome Lachaud <jerome.lachaud@gmail.com>
2017-10-10 17:34:58 +0100

    Destructure props object and and custom class

--- src/Components/Label.js
+++ src/Components/Label.js
@@ -2,17 +2,26 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './Label.css'
 
-export const Label = props => (
+export const Label = props => {
 
-  <label
-    className="search-form-label"
-    htmlFor={props.htmlFor}>
-    {props.text}
-  </label>
+  const {
+    className,
+    htmlFor,
+    text,
+  } = props
 
-)
+  return (
+    <label
+      className={className}
+      htmlFor={htmlFor}>
+      {text}
+    </label>
+  )
+
+}
 
 Label.propTypes = {
   htmlFor: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
+  className: PropTypes.string,
 }
