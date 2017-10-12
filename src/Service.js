93981bcaa6948c81361f71aeb7f0a610507197f1
Jerome Lachaud <jerome.lachaud@gmail.com>
2017-10-12 17:05:13 +0100

    Populate the search domain

--- src/Service.js
+++ src/Service.js
@@ -6,7 +6,7 @@ const baserUrl = 'http://www.vam.ac.uk/api/json/museumobject/search?'
 export const Service = {
   fetch(opt = {}) {
     const imagesOption = isOptionCheked(opt.imagesOption)
-    const url = `${baserUrl}objectnamesearch=${opt.query}&results=10&images=${imagesOption}`
+    const url = `${baserUrl}${opt.domainsOption}=${opt.query}&results=10&images=${imagesOption}`
     return fetch(url)
       .then(response => {
         if (response.status >= 400) {
