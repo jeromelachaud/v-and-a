cc30eb2949b1b2caf01c12341eb3d8b02017eb38
Jerome Lachaud <jerome.lachaud@gmail.com>
2017-10-12 16:11:25 +0100

    Modify, rename and move domains dropdown list content

---
+++ src/constants/domains-option.js
@@ -0,0 +1,18 @@
+export const domainsOption = [
+  {
+    value: 'objectnamesearch',
+    text: 'Object Type and Title',
+  },
+  {
+    value: 'namesearch',
+    text: 'Artist or Maker',
+  },
+  {
+    value: 'placesearch',
+    text: 'Place',
+  },
+  {
+    value: 'materialsearch',
+    text: 'Materials and techniques',
+  },
+]
