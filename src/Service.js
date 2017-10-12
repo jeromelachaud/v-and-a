8d20799e415a55e1051d2f2164e4ca69aab008ba
Jerome Lachaud <jerome.lachaud@gmail.com>
2017-10-12 09:28:24 +0100

    Use the imagesOption bool to set the images query parameter

--- src/Service.js
+++ src/Service.js
@@ -1,10 +1,12 @@
 import fetch from 'isomorphic-fetch'
+import isOptionCheked from './helpers/is-option-checked'
+
 const baserUrl = 'http://www.vam.ac.uk/api/json/museumobject/search?'
 
 export const Service = {
   fetch(opt = {}) {
-    const url = `${baserUrl}objectnamesearch=${opt.query}&results=10`
-
+    const imagesOption = isOptionCheked(opt.imagesOption)
+    const url = `${baserUrl}objectnamesearch=${opt.query}&results=10&images=${imagesOption}`
     return fetch(url)
       .then(response => {
         if (response.status >= 400) {
---
+++ src/helpers/is-option-checked.js
@@ -0,0 +1,3 @@
+export default function isCheked(opt) {
+  return opt ? 1 : 0
+}
