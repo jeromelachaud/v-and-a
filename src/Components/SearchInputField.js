38eee3d326e01521b9ee0c6e5fa029810d6ac420
Jerome Lachaud <jerome.lachaud@gmail.com>
2017-10-14 14:32:59 +0100

    Make sure the search input field is correctly named and referenced

--- src/Components/SearchInputField.js
+++ src/Components/SearchInputField.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Label } from './Label'
 import PropTypes from 'prop-types'
-import './Input.css'
+import './SearchInputField.css'
 
 export const SearchInputField = props => (
   <div>
     <Label
-      htmlFor="searchFormInPutField"
+      htmlFor="SearchInputField"
       text="Search in the Victoria & Albert Museum"/>
 
     <input
@@ -14,8 +14,8 @@ export const SearchInputField = props => (
       size="80"
       type="search"
       aria-label="Search in the Victoria & Albert Museum"
-      id="searchFormInPutField"
-      className="search-form-input-field"
+      id="searchInPutField"
+      className="search-input-field"
       placeholder="Search in the Victoria & Albert Museum"
       onChange={props.onChange}>
     </input>
